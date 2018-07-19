d76f06b6d36bb8aed11d53bc26a9b145830e185c
jiayun-yu <>
2018-07-19 19:34:21 +0900

    continue

--- crawler.py
+++ crawler.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+HOME_URL = 'https://www.nankankeiba.com'
+
 def url_to_soup(url):
     req = requests.get(url)
     return BeautifulSoup(req.content, 'html.parser')
 
-soup = url_to_soup('https://www.platform-one.co.jp/contact/index.php?act=input')
-print(soup.prettify())
+def horse_page_link(url):
+    soup = url_to_soup(url)
+    link_list = [HOME_URL + x.get('href') for x in soup.find_all('a', class_='tx-mid tx-low') ]
+    return link_list
+
+#soup = url_to_soup('https://www.platform-one.co.jp/contact/index.php?act=input')
+#print(soup.prettify())
+
+horse_page_link_list = horse_page_link('https://www.nankankeiba.com/race_info/2018071921050110.do')
+print(horse_page_link_list)
