4b9a4d9a39c6cd70a1986b16e0eea606438bb7ce
富樫　大 <dtogashi@MBP13JAL-NC0025.local>
2020-01-15 20:45:34 +0900

    mod dir path

--- predict.py
+++ predict.py
@@ -1,9 +1,11 @@
-import crawler
-import preprocessing
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-horse_page_link_list = crawler.horse_page_link('https://www.nankankeiba.com/race_info/2018072719040410.do')
-df_horses_data = crawler.horse_data(horse_page_link_list[0])
-#print(df_horses_data)
-pre_df = preprocessing.add_race_data(df_horses_data)
-print(pre_df)
-#hourse_data = preprocessing(df_horses_data, today_condition, race_len)
+import sys
+from src import *
+
+if __name__ == '__main__':
+  horse_page_link_list = crawler.horse_page_link(sys.argv[1])
+  df_horses_data = crawler.horse_data(horse_page_link_list[0])
+  pre_df = preprocessing.add_race_data(df_horses_data)
+  print(pre_df)
